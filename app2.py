a54e8964d2e16f0bfd0b41c29b9419af186cd780
Aluno Lab 135 <lab135@lab.senai>
2023-09-06 16:11:12 -0300

    calc_v2

---
+++ app2.py
@@ -0,0 +1,15 @@
+import os
+
+os.system("cls")
+
+choose=int(input("escolha a operação:('+=1 ou -=2')"))
+if choose ==1:
+    a=int(input("Digite o primeiro valor: "))
+    b=int(input("Digite o segundo valor: "))
+    soma=(a+b)
+    print(soma)
+elif choose ==2:
+    a=int(input("Digite o primeiro valor: "))
+    b=int(input("Digite o segundo valor: "))
+    subtra=(a-b)
+    print("Subtração: ",subtra)
